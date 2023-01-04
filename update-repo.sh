dd2b7f4e1707af446fe9b04c08adea2fe3c54703
Atsushi Watanabe <atsushi.w@ieee.org>
2023-01-04 15:38:10 +0900

    Move diff_uri to narrower scope

--- update-repo.sh
+++ update-repo.sh
@@ -130,11 +130,11 @@ else
       . aports.prev/${file}
       echo "${rosinstall}" | sed -n 's/^\s*version: \(\S*\)$/\1/p'
     )
-    diff_uri="${base_uri}/compare/${old_tag}...${new_tag}"
 
     if [ "${new_tag}" = "${old_tag}" ]; then
       echo "- $(dirname ${file}) no upstream update" >> ${pr_body_file}
     else
+      diff_uri="${base_uri}/compare/${old_tag}...${new_tag}"
       echo "- $(dirname ${file}) [diff](${diff_uri})" >> ${pr_body_file}
     fi
   done
